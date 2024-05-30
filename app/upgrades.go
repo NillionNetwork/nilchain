1c57dcf625bc425a63f9ebe6f847f6c30601683f
Marko Baricevic <markobaricevic3778@gmail.com>
2024-05-30 10:23:17 +0200

    add upgrade information

---
+++ app/upgrades.go
@@ -0,0 +1,38 @@
+package nillionapp
+
+import (
+	"context"
+
+	storetypes "cosmossdk.io/store/types"
+	upgradetypes "cosmossdk.io/x/upgrade/types"
+	metatypes "github.com/NillionNetwork/nilchain/x/meta/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+)
+
+// this upgrade adds the metatypes.StoreKey to the store
+const UpgradeName = "v010-to-v020"
+
+func (app NillionApp) RegisterUpgradeHandlers() {
+	app.UpgradeKeeper.SetUpgradeHandler(
+		UpgradeName,
+		func(ctx context.Context, _ upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+			return app.ModuleManager.RunMigrations(ctx, app.Configurator(), fromVM)
+		},
+	)
+
+	upgradeInfo, err := app.UpgradeKeeper.ReadUpgradeInfoFromDisk()
+	if err != nil {
+		panic(err)
+	}
+
+	if upgradeInfo.Name == UpgradeName && !app.UpgradeKeeper.IsSkipHeight(upgradeInfo.Height) {
+		storeUpgrades := storetypes.StoreUpgrades{
+			Added: []string{
+				metatypes.StoreKey,
+			},
+		}
+		// configure store loader that checks if version == upgradeHeight and applies store upgrades
+		app.SetStoreLoader(upgradetypes.UpgradeStoreLoader(upgradeInfo.Height, &storeUpgrades))
+	}
+
+}
