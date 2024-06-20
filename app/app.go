e110a09885a2f59f0e435133a5311b6815ec3d0d
Elias Rad <146735585+nnsW3@users.noreply.github.com>
2024-06-20 18:36:45 +0300

    fix app.go

--- app/app.go
+++ app/app.go
@@ -240,7 +240,7 @@ func NewNillionApp(
 	std.RegisterLegacyAminoCodec(legacyAmino)
 	std.RegisterInterfaces(interfaceRegistry)
 
-	// Below we could construct and set an application specific mempool and
+	// Below we could construct and set an application-specific mempool and
 	// ABCI 1.0 PrepareProposal and ProcessProposal handlers. These defaults are
 	// already set in the SDK's BaseApp, this shows an example of how to override
 	// them.
