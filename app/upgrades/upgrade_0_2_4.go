301934e9862cc09bded2aeb7f395510c8c4c647b
Randy Grok <randygrok@gmail.com>
2024-07-10 10:29:58 +0200

    upgrade name set to rc11

--- app/upgrades/upgrade_0_2_4.go
+++ app/upgrades/upgrade_0_2_4.go
@@ -9,7 +9,7 @@ import (
 )
 
 var Upgrade_0_2_4 = Upgrade{
-	UpgradeName: "v0.2.4-rc10",
+	UpgradeName: "v0.2.4-rc11",
 	CreateUpgradeHandler: func(mm module.Manager, configurator module.Configurator) upgradetypes.UpgradeHandler {
 		return func(ctx context.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
 			return mm.RunMigrations(ctx, configurator, fromVM)
