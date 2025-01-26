d41b9d93e6d5432eec7b2cf0e51670fcbad1e8d1
jiangmencity <jiangmen@52it.net>
2025-01-26 23:03:14 +0800

    chore: fix some comments (#57)
    Signed-off-by: jiangmencity <jiangmen@52it.net>

--- tests/upgrades/upgrade_test.go
+++ tests/upgrades/upgrade_test.go
@@ -24,7 +24,7 @@ type UpgradeTestSuite struct {
 	common.NetworkTestSuite
 }
 
-// TestUpgrade0_2_1 tests the upgrade from 0.2.1 to 0.2.4
+// TestUpgrade0_2_4 tests the upgrade from 0.2.1 to 0.2.4
 func (s *UpgradeTestSuite) TestUpgrade0_2_4() {
 	oldVersion := "v0.2.1-test-only"
 	newVersion := upgrades.Upgrade_0_2_4.UpgradeName
